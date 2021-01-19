efee25ee5b75ffcf2c073284d40e44a6528ecd6f
Dong Min <adsad1002112@naver.com>
2021-01-19 16:46:58 +0900

    ADD : First Solution

--- basic_math.py
+++ basic_math.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 #######################
 # Basic Math          #
 #######################
@@ -24,7 +26,7 @@ def get_greatest(number_list):
             >>> bm.get_greatest(number_list)
             99
     """
-    greatest_number = None
+    greatest_number = max(number_list)
     return greatest_number
 
 
@@ -45,7 +47,7 @@ def get_smallest(number_list):
             >>> bm.get_smallest(number_list)
             11
     """
-    smallest_number = None
+    smallest_number = min(number_list)
     return smallest_number
 
 
@@ -66,7 +68,7 @@ def get_mean(number_list):
             >>> bm.get_mean(number_list)
             47
     """
-    mean = None
+    mean = sum(number_list) / len(number_list)
     return mean
 
 
@@ -90,5 +92,9 @@ def get_median(number_list):
             >>> bm.get_median(number_list2)
             35.5
     """
-    median = None
+    center = (len(number_list) // 2) - 1
+    if len(number_list) % 2 == 1:
+        median = sorted(number_list)[center + 1]
+    else:
+        median = sum(sorted(number_list)[center:center + 2]) / 2
     return median
